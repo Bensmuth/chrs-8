b98fc8ebea632d7e4525506ffd64164f493da02d
Bensmuth <reocha97@gmail.com>
2020-01-18 19:50:14 +0000

    Added file read and cpu loop logic

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,6 @@
 mod cpu;
+mod display;
+
 
 extern crate sdl2; 
 
@@ -6,32 +8,32 @@ use sdl2::pixels::Color;
 use sdl2::event::Event;
 use sdl2::keyboard::Keycode;
 use std::time::Duration;
+use std::fs::File;
+use std::io::Read;
+use std::collections::LinkedList;
 
-fn windowsetup(sdl_context : &sdl2::Sdl) -> sdl2::render::Canvas<sdl2::video::Window>{
-    let video_subsystem = sdl_context.video().unwrap();
- 
-    let window = video_subsystem.window("rust-sdl2 demo", 800, 600)
-        .position_centered()
-        .build()
-        .unwrap();
- 
-    let mut canvas = window.into_canvas().build().unwrap();
- 
-    canvas.set_draw_color(Color::RGB(0, 0, 0));
-    canvas.clear();
-    canvas.present();
-    return canvas
-}
+
 
 fn main() {
-    cpu::main()
+
+    let mut memory = [0;4096];
+
+    let mut file=File::open("resources/pong.ch8").unwrap(); // ! dirty rom load, replace this when cartridge controller implemented
+    let mut buf=[0u8;0xFF];
+    file.read(&mut buf).unwrap();
+    for x in 0..0xFF { // ! dirty rom into memeory merge, bad method only supports bios at the moment
+        memory[x + 0x200] = buf[x];
+    };
+
+    let mut main_cpu = cpu::Cpu::new(memory, [0;16], LinkedList::new(), [[false; 64]; 32], 0, 0x200);
+
     
     let sdl_context = sdl2::init().unwrap();
-    let mut canvas = windowsetup(&sdl_context);
-    let mut event_pump = sdl_context 
-        canvas.set_draw_color(Color::RGB(0, 0, 0));
+    let mut canvas = display::windowsetup(&sdl_context);
+    let mut event_pump = sdl_context.event_pump().unwrap();
+    'running: loop {
+        canvas.set_draw_color(Color::RGB(0x00, 0x0, 0x0));
         canvas.clear();
-
         for event in event_pump.poll_iter() {
             match event {
                 Event::Quit {..} |
@@ -42,11 +44,13 @@ fn main() {
             }
         }
         // The rest of the game loop goes here...
+        main_cpu.tick();
         canvas.set_draw_color(Color::RGB(0xFF, 0xFF, 0xFF));
         let rect = sdl2::rect::Rect::new(100,100,200,200);
 
-        canvas.fill_rect(rect);
+        canvas.fill_rect(rect).map_err(|err| println!("{:?}", err)).ok();
 
         canvas.present();
         ::std::thread::sleep(Duration::new(0, 1_000_000_000u32 / 60));
-    }}
+    }
+}
