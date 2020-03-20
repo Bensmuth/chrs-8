e94b938be118398c25e039af7901e390c6d0679c
Bensmuth <reocha97@gmail.com>
2020-03-20 17:27:04 +0000

    Implemented/Fixed some opcodes and drawing,

--- src/cpu.rs
+++ src/cpu.rs
@@ -1,14 +1,19 @@
 use std::process::exit;
 use std::collections::LinkedList;
 use rand::prelude::*;
+use std::thread;
+use std::time;
 
 pub struct Cpu {
     mem: [u8;4096],
     reg: [u8;16], //v0 to VF
     stack: LinkedList<u16>,
-    gfx: [[bool; 64]; 32],
+    pub gfx: [[bool; 64]; 32],
     i: u16, //address register
     pc: u16, // program counter, should really only be u12
+    tdelay: u8,
+    tsound: u8,
+    epoch : time::Instant,
 }
 
 impl Cpu {
@@ -28,31 +33,57 @@ impl Cpu {
             gfx,
             i,
             pc,
+            tdelay : 0,
+            tsound : 0,
+            epoch : time::Instant::now()
         }
+    }
+
+    pub fn debug(&mut self){
+        self.reg[0] = 0b1111_1111;
+        self.reg[1] = 0b0000_0001
     }
     
     fn pcc(&mut self) -> u8{ //program counter call, use this whenever iterating pc and wanting to get something from memory in that iteration
+        if self.pc > 4096{
+            println!("Error, went over mem!");
+            exit(0);
+        }
         let v = self.mem[self.pc as usize];
         self.pc += 1;
         v
     }
 
+    fn timers(&mut self){
+        if self.tdelay != 0{
+            self.tdelay -= 1;
+        } else if self.tsound != 0{
+            self.tsound -= 1;
+        }
+    }
+
     pub fn tick(&mut self){
+        if self.epoch.elapsed().as_millis() % 17 <= 3{
+            self.timers();
+        }
         let opcode : u8 = self.pcc(); 
         match (opcode >> 4) {
             0x0 => {
-                if self.pcc() == 0xE0{
+                let oprand = self.pcc();
+                if oprand == 0xE0{
                     self.gfx = [[false; 64]; 32];
-                } else{
+                } else if oprand == 0xEE{
                     self.pc = self.stack.pop_back().unwrap_or(0x200);
+                } else{
+                    println!("Wack")
                 }
             },
             0x1 =>{
-                self.pc = (opcode as u16) << 8 | self.pcc() as u16;
+                self.pc = ((opcode & 0x0F) as u16) << 8 | self.pcc() as u16;
             },
             0x2 =>{
                 self.stack.push_back(self.pc);
-                self.pc = (opcode as u16) << 8 | self.pcc() as u16;
+                self.pc = ((opcode & 0x0F) as u16) << 8 | self.pcc() as u16;
             },
             0x3 =>{
                 if self.reg[(opcode & 0x0F) as usize] == self.pcc(){
@@ -67,7 +98,7 @@ impl Cpu {
                 }
             },
             0x5 =>{
-                if self.reg[(opcode & 0x0F) as usize] == self.reg[(self.pcc() & 0xF0) as usize]{
+                if self.reg[(opcode & 0x0F) as usize] == self.reg[((self.pcc() & 0xF0) >> 4) as usize]{
                     self.pcc();
                     self.pcc();
                 }
@@ -76,25 +107,26 @@ impl Cpu {
                 self.reg[(opcode & 0x0F) as usize] = self.pcc();
             },
             0x7 =>{
-                self.reg[(opcode & 0x0F) as usize] += self.pcc();
+                let value = self.pcc();
+                    self.reg[(opcode & 0x0F) as usize] = ((self.reg[(opcode & 0x0F) as usize] as u16 + value as u16) % 0x100) as u8; // handles overflow correctly?
             },
             0x8 =>{
                 let oprand = self.pcc();
                 match oprand & 0x0F {
                     0x0 =>{
-                        self.reg[(opcode & 0x0F) as usize] = self.reg[(oprand & 0xF0) as usize]
+                        self.reg[(opcode & 0x0F) as usize] = self.reg[((oprand & 0xF0) >> 4) as usize]
                     },
                     0x1 =>{
-                        self.reg[(opcode & 0x0F) as usize] = self.reg[(oprand & 0xF0) as usize] | self.reg[(oprand & 0xF0) as usize]
+                        self.reg[(opcode & 0x0F) as usize] = self.reg[((opcode & 0x0F)) as usize] | self.reg[((oprand & 0xF0) >> 4) as usize]
                     },
                     0x2 =>{
-                        self.reg[(opcode & 0x0F) as usize] = self.reg[(oprand & 0xF0) as usize] & self.reg[(oprand & 0xF0) as usize]
+                        self.reg[(opcode & 0x0F) as usize] = self.reg[((opcode & 0x0F)) as usize] & self.reg[((oprand & 0xF0) >> 4) as usize]
                     },
                     0x3 =>{
-                        self.reg[(opcode & 0x0F) as usize] = self.reg[(oprand & 0xF0) as usize] ^ self.reg[(oprand & 0xF0) as usize]
+                        self.reg[(opcode & 0x0F) as usize] = self.reg[((opcode & 0x0F)) as usize] ^ self.reg[((oprand & 0xF0) >> 4) as usize]
                     },
                     0x4 =>{
-                        let addition = self.reg[(oprand & 0xF0) as usize].overflowing_add(self.reg[(oprand & 0xF0) as usize]);
+                        let addition = self.reg[(opcode & 0x0F) as usize].overflowing_add(self.reg[((oprand & 0xF0) >> 4) as usize]);
                         self.reg[(opcode & 0x0F) as usize] = addition.0;
                         if addition.1{
                             self.reg[0xF] = 1
@@ -103,7 +135,7 @@ impl Cpu {
                         }
                     },
                     0x5 =>{
-                        let subtract = self.reg[(oprand & 0xF0) as usize].overflowing_sub(self.reg[(oprand & 0xF0) as usize]);
+                        let subtract = self.reg[(opcode & 0x0F) as usize].overflowing_sub(self.reg[((oprand & 0xF0) >> 4) as usize]);
                         self.reg[(opcode & 0x0F) as usize] = subtract.0;
                         if subtract.1{
                             self.reg[0xF] = 0
@@ -116,7 +148,7 @@ impl Cpu {
                         self.reg[(opcode & 0x0F) as usize] >>= 1;
                     },
                     0x7 =>{
-                        let subtract = self.reg[(oprand & 0xF0) as usize].overflowing_sub(self.reg[(oprand & 0xF0) as usize]);
+                        let subtract = self.reg[((oprand & 0xF0) >> 4) as usize].overflowing_sub(self.reg[(opcode & 0x0F) as usize]);
                         self.reg[(opcode & 0x0F) as usize] = subtract.0;
                         if subtract.1{
                             self.reg[0xF] = 0
@@ -135,7 +167,7 @@ impl Cpu {
             },
             0x9 =>{
                 let oprand = self.pcc();
-                if self.reg[(opcode & 0x0F) as usize] != self.reg[(oprand & 0xF0) as usize]{
+                if self.reg[(opcode & 0x0F) as usize] != self.reg[(oprand & 0xF0 >> 4) as usize]{
                     self.pcc();
                     self.pcc();
                 }
@@ -156,13 +188,69 @@ impl Cpu {
                 self.reg[(opcode & 0x0F) as usize] = oprand & rand;
             },
             0xD =>{
-
+                let oprand = self.pcc();
+                let x = ((opcode) & 0x0F);
+                let y = (oprand & 0xF0) >> 4;
+                let n = oprand & 0x0F;
+                for px in x..(x+8){
+                    for py in y..(y+n){
+                        if self.gfx[py as usize][px as usize]{
+                            self.reg[0xF] = 1;
+                            self.gfx[py as usize][px as usize] = false;
+                        } else{
+                            self.gfx[py as usize][px as usize] = true;
+                        }
+                    }
+                }
             },
-            0xE =>{
+            0xE =>{ //keyops
 
             },
             0xF =>{
+                let oprand = self.pcc();
+                match oprand{
+                    0x07=>{
+                        self.reg[((opcode) & 0x0F) as usize] = self.tdelay;
+                    },
+                    0x0A=>{ //blocking keyop
 
+                    },
+                    0x15=>{
+                        self.tdelay = self.reg[((opcode) & 0x0F) as usize];
+                    },
+                    0x18=>{
+                        self.tsound = self.reg[((opcode) & 0x0F) as usize];
+                    },
+                    0x1E=>{
+                        self.i += self.reg[((opcode) & 0x0F) as usize] as u16;
+                        if self.i > 0xfff{
+                            self.i = 0;
+                            self.reg[0xF] = 1;
+                        } else {
+                            self.reg[0xF] = 0;
+                        }
+                    },
+                    0x29=>{ //char loads
+                        self.i = 0;
+                    },
+                    0x33=>{
+                        let bcd = (opcode) & 0x0F;
+                        self.mem[self.i as usize] = bcd / 100;
+                        self.mem[(self.i + 1) as usize] = (bcd%100)/10;
+                        self.mem[self.i as usize] = bcd % 10;
+
+                    }
+                    0x65=>{
+                        let x = opcode & 0x0F;
+                        for z in 0..x{
+                            self.reg[z as usize] = self.mem[(self.i + z as u16) as usize]
+                        }
+                    }
+                    _ =>{
+                        println!("Not implemented {:x}", oprand);
+                        exit(0)
+                    }
+                }
             },
 
             _ => {
--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
 mod cpu;
 mod display;
-
+// !! https://github.com/DavidJowett/chip8-emulator !! got deets on cpu instructions
 
 extern crate sdl2; 
 
@@ -18,14 +18,15 @@ fn main() {
 
     let mut memory = [0;4096];
 
-    let mut file=File::open("resources/pong.ch8").unwrap(); // ! dirty rom load, replace this when cartridge controller implemented
+    let mut file=File::open("resources/test_opcode.ch8").unwrap(); // ! dirty rom load, replace this when cartridge controller implemented
     let mut buf=[0u8;0xFF];
     file.read(&mut buf).unwrap();
-    for x in 0..0xFF { // ! dirty rom into memeory merge, bad method only supports bios at the moment
+    for x in 0..0xFF { // ! dirty rom into memeory merge, probs wont ever fix this
         memory[x + 0x200] = buf[x];
     };
 
     let mut main_cpu = cpu::Cpu::new(memory, [0;16], LinkedList::new(), [[false; 64]; 32], 0, 0x200);
+    main_cpu.debug(); // ! make modifications to registers for testing
 
     
     let sdl_context = sdl2::init().unwrap();
@@ -46,9 +47,16 @@ fn main() {
         // The rest of the game loop goes here...
         main_cpu.tick();
         canvas.set_draw_color(Color::RGB(0xFF, 0xFF, 0xFF));
-        let rect = sdl2::rect::Rect::new(100,100,200,200);
+        for x in 0..63{ // gfx drawing
+            for y in 0..31{
+                if main_cpu.gfx[y][x] == true{
+                    let rect = sdl2::rect::Rect::new((x * 10) as i32,(y*10) as i32,10,10);
+                    canvas.fill_rect(rect).map_err(|err| println!("{:?}", err)).ok();
+                }
+            } 
+        }
 
-        canvas.fill_rect(rect).map_err(|err| println!("{:?}", err)).ok();
+
 
         canvas.present();
         ::std::thread::sleep(Duration::new(0, 1_000_000_000u32 / 60));
